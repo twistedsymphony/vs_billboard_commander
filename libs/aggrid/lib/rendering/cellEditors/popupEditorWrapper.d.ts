c0105321dc24605240c4e9c03bde7a9d5786a972
twistedsymphony <michael.pica@gmail.com>
2017-06-03 10:56:02 -0400

    Add files via upload

---
+++ libs/aggrid/lib/rendering/cellEditors/popupEditorWrapper.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for ag-grid v9.1.0
+// Project: http://www.ag-grid.com/
+// Definitions by: Niall Crosby <https://github.com/ceolter/>
+import { Component } from "../../widgets/component";
+import { ICellEditorComp, ICellEditorParams } from "./iCellEditor";
+export declare class PopupEditorWrapper extends Component implements ICellEditorComp {
+    private cellEditor;
+    private params;
+    private getGuiCalledOnChild;
+    constructor(cellEditor: ICellEditorComp);
+    private onKeyDown(event);
+    getGui(): HTMLElement;
+    init(params: ICellEditorParams): void;
+    afterGuiAttached(): void;
+    getValue(): any;
+    isPopup(): boolean;
+    isCancelBeforeStart(): boolean;
+    isCancelAfterEnd(): boolean;
+    focusIn(): void;
+    focusOut(): void;
+}
---
+++ libs/aggrid/lib/widgets/popupService.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for ag-grid v9.1.0
+// Project: http://www.ag-grid.com/
+// Definitions by: Niall Crosby <https://github.com/ceolter/>
+export declare class PopupService {
+    private gridCore;
+    private gridOptionsWrapper;
+    private getPopupParent();
+    positionPopupForMenu(params: {
+        eventSource: any;
+        ePopup: HTMLElement;
+    }): void;
+    positionPopupUnderMouseEvent(params: {
+        mouseEvent: MouseEvent | Touch;
+        ePopup: HTMLElement;
+    }): void;
+    positionPopupUnderComponent(params: {
+        eventSource: HTMLElement;
+        ePopup: HTMLElement;
+        minWidth?: number;
+        nudgeX?: number;
+        nudgeY?: number;
+        keepWithinBounds?: boolean;
+    }): void;
+    positionPopupOverComponent(params: {
+        eventSource: HTMLElement;
+        ePopup: HTMLElement;
+        minWidth?: number;
+        nudgeX?: number;
+        nudgeY?: number;
+        keepWithinBounds?: boolean;
+    }): void;
+    private positionPopup(params);
+    private keepYWithinBounds(params, y);
+    private keepXWithinBounds(params, x);
+    addAsModalPopup(eChild: any, closeOnEsc: boolean, closedCallback?: () => void): (event?: any) => void;
+}
